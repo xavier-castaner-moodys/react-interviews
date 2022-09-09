ce7bd8eb3ff8f394e43099386acdfdf6f73098e2
Xavier Castaner <xavier.castaner@moodys.com>
2022-09-09 10:34:52 +0200

    api info

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
 function App() {
+  // api: https://pokeapi.co/api/v2/pokemon/1
+  // name:string
+  // abilities: {abilities: name}[]
+  
   return (
     <main>
       <header><h1>Pokemon card random</h1></header>
