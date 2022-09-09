d07d20b13a8a6d20c2f1e764b43005a005085667
Xavier Castaner <xavier.castaner@moodys.com>
2022-09-09 10:32:23 +0200

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <main>
+      <header><h1>Pokemon card random</h1></header>
+      <button>get random pokemon</button>
+      <article></article>
+    </main>
+
+  )
+}
+
+export default App
